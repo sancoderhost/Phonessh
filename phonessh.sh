26a534758a013cb04936137bbc631456b8b0998b
sancoderhost <sancoder998@gmail.com>
2023-09-27 19:32:17 +0530

    Added firstcheck

--- phonessh.sh
+++ phonessh.sh
@@ -1,23 +1,55 @@
+#!/bin/bash 
+source ./main.conf 
+host=""
+first_start()
+{
+				if [[ ! -e ./firststart  ]];
+				then 
+						echo 1 > ./firststart 
+						#scp -i "$IDENTITY_KEY" -P "$SSHPORT" ./mdns.sh "$host:/data/data/com.termux/files/home/mdns.sh"
+						CONFIG_FILE="avahi-daemon.conf"  # Update this path as needed
+						# Save the current value of IFS to restore it later
+						oldIFS=$IFS
+						# Set IFS to '.' to split the string by dots
+						IFS='.'
+						read -ra hostvalue <<< "$HOSTDOMAIN"
+						# Restore the original value of IFS
+						IFS=$oldIFS
+
+						# Check if the configuration file exists
+						if [ ! -f "$CONFIG_FILE" ]; then
+						  echo "Avahi configuration file not found: $CONFIG_FILE"
+						  exit 1
+						fi
+						
+						# Use sed to insert the host and domain values into the configuration file
+						sed -i "s/^host-name=.*/host-name=${hostvalue[0]}/" "$CONFIG_FILE"
+						sed -i "s/^domain-name=.*/domain-name=${hostvalue[1]}/" "$CONFIG_FILE"
+						
+						echo "Host and domain values updated in $CONFIG_FILE"
+						scp -i $IDENTITY_KEY  -P $SSHPORT $CONFIG_FILE  $host:/data/data/com.termux/files/usr/etc/avahi/
+						#scp -i $IDENTITY_KEY -P $SSHPORT ./avahi-daemon.conf $host:/data/data/com.termux/files/usr/etc/avahi/
+						
+						
+				fi
+}
 phone_ssh()
 {
 
-		source ./main.conf 
+
+
         #idkey='/home/sanbotbtrfs/phoneid'
 		checkflag=0
 		echo $HOSTDOMAIN
         ping -c 1 $HOSTDOMAIN >/dev/null 2>&1
         if [[ $? -ne 0 ]] 
         then
+				first_start
                 if host=$(ip neigh  | awk   '/([0-9]{1,3}\.){3}[0-9]{1,3}/ {print $1}' |nmap -p $SSHPORT  --open   -iL   -  |grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' )
                 then
 
                         echo 'mdns failed ,bruteforce fallback'
 
-				if [[ ! -e ./firststart  ]];
-				then 
-						echo 1 > ./firststart 
-						scp -i $IDENTITY_KEY -P $SSHPORT ./mdns.sh $host:/data/data/com.termux/files/home/mdns.sh
-				fi
 						ssh -i $IDENTITY_KEY -p $SSHPORT  $host bash /data/data/com.termux/files/home/mdns.sh
                 else
                         echo 'no server'
@@ -25,8 +57,9 @@ phone_ssh()
 						checkflag=1
                 fi
         else
-                echo 'mdns ,  $HOSTDOMAIN  detected'
+                echo "mdns ,  $HOSTDOMAIN  detected"
                 host=$HOSTDOMAIN
+				first_start
 				if !  nmap -p $SSHPORT --open $host | grep -ow $SSHPORT  >/dev/null  2>&1 
 				then
 						checkflag=1
